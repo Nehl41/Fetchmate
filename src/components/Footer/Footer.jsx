fdb2c7c3d6c24120fc29a42678ce46c573b75693
Swayam101 <swayamprajapat21@gmail.com>
2023-04-07 13:20:12 +0530

    Git error Resloved

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import './footer.css';
-import NehalImg from './developerImg/Nehal.jpg'
+import NehalImg from '../../assets/Image/CardParent.jpg'
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TwitterIcon from '@mui/icons-material/Twitter';
