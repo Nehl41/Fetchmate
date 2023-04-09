83f5da32cf1d29dcb50fbfca5edb2e3c6dfdb271
nehl41 <nehu.np20@gmail.com>
2023-04-10 01:12:36 +0530

    petParent updated

--- src/Pages/PetParent/Parent.jsx
+++ src/Pages/PetParent/Parent.jsx
@@ -5,10 +5,10 @@ import petFrame from '../../assets/Image/Frame1.png'
 const petParent = () => {
     return(
         <div className='parent-container d-flex'>
-            <div className='parent-header max-width-100'>
-                <div className='parent-header-content'>This might be your pet when you leave them home?<br></br>
-                You may have also thought of daycares but didn't found a reliable one<br></br>
-                where you could track you day anytime or you may be unsure of there proper care.
+            <div className='parent-header max-width-100 '>
+                <div className='parent-header-content'><h1>This might be your pet when you leave them home?</h1><br></br>
+                <h5>You may have also thought of daycares but didn't found a reliable one,
+                where you could track you day anytime or you may be unsure of there proper care.</h5>
                 </div>
                 <div className='parent-header-frame'><img src={petFrame}></img></div>
             </div>
