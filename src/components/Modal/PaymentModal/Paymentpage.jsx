71edb7a7e6e6afca8a88e6219c239c6735834724
nehl41 <nehu.np20@gmail.com>
2023-05-04 10:16:46 +0530

    added payment page

--- src/components/Modal/PaymentModal/Paymentpage.jsx
+++ src/components/Modal/PaymentModal/Paymentpage.jsx
@@ -5,6 +5,37 @@ import { useState } from 'react';
 
 
 const paymentmodal = ({showpaymentModal , setpaymentShowModal}) => {
+  // const [paymentMode, setPaymentMode] = useState('UPI'); // Default to UPI mode
+  // const [cardType, setCardType] = useState('');
+  // const [cardNumber, setCardNumber] = useState('');
+  // const [mobileNumber, setMobileNumber] = useState('');
+  // const [upiMode, setUpiMode] = useState('PhonePe'); // Default to PhonePe UPI mode
+
+  const handlePaymentModeChange = (e) => {
+    setPaymentMode(e.target.value);
+  };
+
+  const handleCardTypeChange = (e) => {
+    setCardType(e.target.value);
+  };
+
+  const handleCardNumberChange = (e) => {
+    setCardNumber(e.target.value);
+  };
+
+  const handleMobileNumberChange = (e) => {
+    setMobileNumber(e.target.value);
+  };
+
+  const handleUpiModeChange = (e) => {
+    setUpiMode(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission here
+  };
+
    return(
         <Rodal customMaskStyles={{
             backgroundImage: "linear-gradient(to bottom right, grey, white)",
@@ -20,6 +51,65 @@ const paymentmodal = ({showpaymentModal , setpaymentShowModal}) => {
           }}
           visible={showpaymentModal}
           onClose={() => setpaymentShowModal(false)}>
+
+<form onSubmit={handleSubmit}>
+      <div>
+        <label>
+          <input
+            type="radio"
+            name="paymentMode"
+            value="UPI"
+            checked={paymentMode === 'UPI'}
+            onChange={handlePaymentModeChange}
+          />
+          UPI
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="paymentMode"
+            value="Card"
+            checked={paymentMode === 'Card'}
+            onChange={handlePaymentModeChange}
+          />
+          Card
+        </label>
+      </div>
+      {paymentMode === 'Card' && (
+        <div>
+          <label>
+            Card Type:
+            <select value={cardType} onChange={handleCardTypeChange}>
+              <option value=""></option>
+              <option value="Visa">Visa</option>
+              <option value="Mastercard">Mastercard</option>
+            </select>
+          </label>
+          <label>
+            Card Number:
+            <input type="text" value={cardNumber} onChange={handleCardNumberChange} />
+          </label>
+        </div>
+      )}
+      {paymentMode === 'UPI' && (
+        <div>
+          <label>
+            Mobile Number:
+            <input type="text" value={mobileNumber} onChange={handleMobileNumberChange} />
+          </label>
+          <label>
+            UPI Mode:
+            <select value={upiMode} onChange={handleUpiModeChange}>
+              <option value="PhonePe">PhonePe</option>
+              <option value="Paytm">Paytm</option>
+              <option value="GooglePay">Google Pay</option>
+            </select>
+          </label>
+        </div>
+      )}
+      <button type="submit">Submit</button>
+    </form>
+
           </Rodal>
     );
 }
